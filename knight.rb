60bd22266974a5d1c9398d3e848911527223e770
Sergio Guerra <sergioguerra6188@gmail.com>
2023-04-15 15:44:50 -0400

    Add implementation to #knight_moves method
    Added selection logic because this method will use recursion to find the
    quickest route from the startingPoint to destinationPoint.

--- knight.rb
+++ knight.rb
@@ -5,5 +5,18 @@ class Knight
   end
 
   def knight_moves(startingPoint, destinationPoint, currentPath = [])
+    @x = startingPoint[0]
+    @y = startingPoint[1]
+
+    if (currentPath.any?(startingPoint))
+      return
+    elsif (@x < 0 || @x > 7 || @y < 0 || @y > 7)
+      return
+    elsif (startingPoint == destinationPoint)
+      return
+    else
+      # to be implemented
+      return
+    end
   end
 end
